c8a99c59f9d3750eab262077764b19be80c863e0
Elivan Kook <elivanmetal@gmail.com>
2019-02-07 14:53:45 +0100

    done with step 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# quotes
+# Quotes - code challange
+
+* Step 1 -  Use symfony latest version and uplaod the project in GitHab. The URL /api/motd will display a random quote of famous developers from json file. Git-Tag as version 1.0.0.
---
+++ src/Controller/QuoteController.php
@@ -0,0 +1,25 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class QuoteController extends AbstractController {
+        /**
+         * @Route("/api/motd", name="app_motd")
+         */
+
+         # The controller is the number method
+         public function motd(){
+            # Get the contents of the json file
+             $quotesJson = file_get_contents('../var/public/quotes.json');
+            # Convert the json to a php array
+            $arr = json_decode($quotesJson, true);
+            # Randomly pick and element from the array
+            $elem = $arr[mt_rand(0, count($arr) - 1)];
+            # Rturn the random element in json
+             return new JsonResponse($elem);
+         }
+    }
